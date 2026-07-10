4caa53a9341e014752c95d691a3566497fdbd0e7
gtbhj03-ctrl <gtbhj03@gmail.com>
2026-07-10 20:54:23 +0700

    NgocHao.py

---
+++ NgocHao.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Trang web đầu tiên của Ngọc Hào")
+st.write("Xin chào thế giới! Tôi là học sinh chuyên Tin trường Nguyễn Quang Diệu và đây là trang web bằng Python đầu tay của tôi.")
